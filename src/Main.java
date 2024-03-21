353cb74bdeb8c9ff554f00877ac7628707959b9c
PIVAB <155201854+PIVAB@users.noreply.github.com>
2024-03-21 20:41:52 +0300

    Вариант 2

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,75 @@
-import java.util.List;
+import ru.yandex.practicum.kanban.entities.Epic;
+import ru.yandex.practicum.kanban.entities.Status;
+import ru.yandex.practicum.kanban.entities.SubTask;
+import ru.yandex.practicum.kanban.entities.Task;
+import ru.yandex.practicum.kanban.manager.TaskManager;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        TaskManager tracker = new TaskManager();
+        ArrayList<Integer> tasIdList = new ArrayList<>();
+        ArrayList<Integer> subTasIdList = new ArrayList<>();
+        ArrayList<Integer> epicIdList = new ArrayList<>();
 
-        Epic epic = new Epic("Создание нового продукта", "Разработка нового продукта c нуля");
+        // создаем 2 отдельные задачи
+        tasIdList.add(tracker.createTask(new Task("T-1", "Задача 1")));
+        tasIdList.add(tracker.createTask(new Task("T-2", "Задача 2")));
 
-        Subtask subtask1 = new Subtask("Подготовить макет дизайна", "Создание макета интерфейса приложения", epic);
-        Subtask subtask2 = new Subtask("Написать тестовый код", "Кодирование модульных тестов", epic);
+        // создаем эпик с 2-мя подзадачами
+        epicIdList.add(tracker.createEpic(new Epic("E-1", "Эпик 1")));
 
-        epic.addSubtask(subtask1);
-        epic.addSubtask(subtask2);
+        subTasIdList.add(tracker.createSubTask(new SubTask("ST-1", "Подзадача 1", epicIdList.get(0))));
+        subTasIdList.add(tracker.createSubTask(new SubTask("ST-2", "Подзадача 2", epicIdList.get(0))));
 
-        taskManager.createTask(epic);
-        taskManager.createTask(subtask1);
-        taskManager.createTask(subtask2);
+        // создаем эпик с 1-й подзадачей
+        epicIdList.add(tracker.createEpic(new Epic("E-2", "Эпик 2")));
+        subTasIdList.add(tracker.createSubTask(new SubTask("ST-3", "Подзадача 3", epicIdList.get(1))));
 
-        // Пример обновления статуса задачи
-        subtask1.setStatus(TaskStatus.IN_PROGRESS);
+        System.out.println(("Кейс 1. Создание задач."));
+        printAll(tracker);
 
-        // Получение списка всех задач
-        List<Task> allTasks = taskManager.getAllTasks();
-        for (Task task : allTasks) {
-            System.out.println("ID: " + task.getTaskId() + ", Name: " + task.getName() +
-                    ", Status: " + task.getStatus());
+        // Меняем статус задач на IN_PROGRESS
+        for (Task task : tracker.getAllTasks()) {
+            tracker.modifyTask(new Task(task.getName(), task.getDescription(), task.getId(), Status.IN_PROGRESS));
         }
-
-        // Получение подзадач конкретного эпика
-        List<Subtask> epicSubtasks = taskManager.getSubtasksByEpic(epic);
-        System.out.println("Subtasks of Epic: " + epic.getName());
-        for (Subtask subtask : epicSubtasks) {
-            System.out.println("ID: " + subtask.getTaskId() + ", Name: " + subtask.getName() +
-                    ", Status: " + subtask.getStatus());
+        // Меняем статус нечетных подзадач на DONE, а четных - на IN_PROGRESS
+        for (SubTask subTask : tracker.getAllSubTasks()) {
+            if (subTask.getId() % 2 == 0) {
+                tracker.modifySubTask(new SubTask(subTask.getName(), subTask.getDescription(), subTask.getId(),
+                        subTask.getEpicId(), Status.IN_PROGRESS));
+            } else {
+                tracker.modifySubTask(new SubTask(subTask.getName(), subTask.getDescription(), subTask.getId(),
+                        subTask.getEpicId(), Status.DONE));
+            }
         }
+        // меняем описание у эпиков
+        int i = 0;
+        for (Epic epic : tracker.getAllEpics()) {
+            i++;
+            epic.setDescription("Эпик_дескрипшн " + i);
+        }
+        System.out.println(("Кейс 2. Изменение статусов."));
+        printAll(tracker);
+
+        // Удаляем задачу, подзадачу и эпик
+        tracker.deleteTask(tracker.getAllTasks().get(0).getId());
+        // удалим 2-й эпик
+        tracker.deleteEpic(tracker.getAllEpics().get(1).getId());
+        tracker.deleteSubTask(tracker.getAllSubTasks().get(0).getId());
+        System.out.println(("Кейс 3. Удаление задачи, эпика, подзадачи."));
+        printAll(tracker);
+
+        // Проверим метод получения сабтасков по эпику
+        System.out.println("Кейс 4. Выводим на печать все подзадачи эпика.");
+        System.out.println(tracker.getSubTasksByEpic(tracker.getAllEpics().get(0).getId()));
+    }
+
+    public static void printAll(TaskManager tracker) {
+        // Печатаем список всех задач
+        System.out.println("Список всех задач: " + tracker.getAllTasks());
+        System.out.println("Список всех эпиков: " + tracker.getAllEpics());
+        System.out.println("Список всех подзадач: " + tracker.getAllSubTasks());
     }
 }
